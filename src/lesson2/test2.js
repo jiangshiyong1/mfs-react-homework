3c984c54888b722cbfb9be60b5d2e0e804ae0ec6
jaingshiyong1 <1065089472@qq.com>
2018-05-26 11:01:43 +0800

    修改后提交

--- src/lesson2/test2.js
+++ src/lesson2/test2.js
@@ -1,1 +1,11 @@
 // 请使用解构语法实现获取斐波那契数列前10个数
+function* fib(){
+    let a = 0;
+    let b = 1;
+    while(true){
+      [a,b]= [b,a+b]
+      yield a
+    }
+  }
+var [a,b,c,d,e,f,g,h,i,j]=fib()
+console.log([a,b,c,d,e,f,g,h,i,j])
